07bec36171129efdd3e053214585e9d6b58a84c9
iamsid47 <siddheshkulthe43@gmail.com>
2023-04-23 15:30:37 +0530

    added gpt-3

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ import pandas as pd
 load_dotenv()
 OPEN_AI_API = os.getenv("OPEN_AI_API")
 
-df = pd.read_csv("booth.ai.csv")
-print(f"Sample Heading => \n{df.head()}")
+prompt = "What is Kulthe Media?"
+print(prompt)
+response = openai.Completion.create(
+    model="davinci:ft-kulthe-media-2023-04-08-19-23-11",
+    prompt=prompt,
+    stop=".",
+    temperature=0.5,
+    presence_penalty=0,
+    frequency_penalty=0,
+    max_tokens=256,
+)
+
+print(response["choices"][0]["text"])
