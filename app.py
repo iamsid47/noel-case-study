1454ea12ec67d89f13a35d822709571c1d3d5090
iamsid47 <siddheshkulthe43@gmail.com>
2023-04-23 15:41:10 +0530

    added flask app and created an API

--- app.py
+++ app.py
@@ -1,21 +1,39 @@
 import openai
 from dotenv import load_dotenv
 import os
-import pandas as pd
+from flask import Flask, Response, request, jsonify
+import flask_cors
 
 load_dotenv()
 OPEN_AI_API = os.getenv("OPEN_AI_API")
 
-prompt = "What is Kulthe Media?"
-print(prompt)
-response = openai.Completion.create(
-    model="davinci:ft-kulthe-media-2023-04-08-19-23-11",
-    prompt=prompt,
-    stop=".",
-    temperature=0.5,
-    presence_penalty=0,
-    frequency_penalty=0,
-    max_tokens=256,
-)
 
-print(response["choices"][0]["text"])
+def Completion(prompt):
+    global response
+    response = openai.Completion.create(
+        model="davinci:ft-kulthe-media-2023-04-08-19-23-11",
+        prompt=prompt,
+        stop=".",
+        temperature=0.5,
+        presence_penalty=0,
+        frequency_penalty=0,
+        max_tokens=256,
+    )
+
+    print(response["choices"][0]["text"])
+
+
+app = Flask(__name__)
+
+
+@app.route("/resp", methods=["POST"])
+def resp():
+    data = request.get_json()
+    prompt = data["prompt"]
+    Completion(prompt)
+
+    return jsonify({"response": response["choices"][0]["text"] + "."})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
